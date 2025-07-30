a9d17ededb98007c64a233368e879c455d77d8a6
David Manley <davidmanley@Davids-MacBook-Air-2.local>
2025-07-29 17:21:23 -0700

    feat: add MealIngredient helper model

---
+++ lib/models/meal_ingredient.dart
@@ -0,0 +1,23 @@
+// lib/models/meal_ingredient.dart
+import 'ingredient.dart';
+
+/// A wrapper that links an Ingredient with the chosen weight for this meal.
+class MealIngredient {
+  final Ingredient ingredient;
+  double weight; // grams
+
+  MealIngredient({
+    required this.ingredient,
+    required this.weight,
+  });
+
+  /// Recalculate macros based on current weight.
+  double get calories =>
+      ingredient.calories * weight / ingredient.defaultWeight;
+  double get carbs =>
+      ingredient.carbs * weight / ingredient.defaultWeight;
+  double get protein =>
+      ingredient.protein * weight / ingredient.defaultWeight;
+  double get fat =>
+      ingredient.fat * weight / ingredient.defaultWeight;
+}
