8d1217def70baf8f994a9e5700e21c8d13dc0de0
maximumsofty <david.j.manley@gmail.com>
2026-01-12 07:31:05 -0500

    Deploy web build (mobile tweaks)

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:meal_planner_app_local_version/main.dart';
-
-void main() {
-  testWidgets('app builds', (tester) async {
-    await tester.pumpWidget(const MealPlannerApp());
-    expect(find.text('Meal Planner'), findsOneWidget);
-  });
-}
